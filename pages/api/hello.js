06342e4f9620c26a187475ff7be7870b1cdf3ba2
Michal Miszczyszyn <michal@mmiszy.pl>
2022-08-30 15:17:03 +0200

    Add vars

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(_req, res) {
+  res.status(200).json({
+    "process.env.TEST_VARIABLE": process.env.TEST_VARIABLE,
+    "process.env.NEXT_PUBLIC_TEST_VARIABLE": process.env.NEXT_PUBLIC_TEST_VARIABLE,
+    "process.env.TEST_COPY": process.env.TEST_COPY,
+    "process.env.NEXT_PUBLIC_TEST_COPY": process.env.NEXT_PUBLIC_TEST_COPY,
+    "process.env.TEST_INTERPOLATION": process.env.TEST_INTERPOLATION,
+    "process.env.NEXT_PUBLIC_TEST_INTERPOLATION": process.env.NEXT_PUBLIC_TEST_INTERPOLATION,
+  });
 }
